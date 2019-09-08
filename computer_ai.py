c826537d368115fc76b56c3fc19d96247630f292
Michael Stein <Michael.b.d.Stein@gmail.com>
2019-09-08 14:24:24 -0400

    Added alpha-beta optimization, the ability to run from any marker ('X', 'O' or otherwise), and a wrapper function to call minimax, which saves and returns the coordinates of the optimal move at the root of the game tree.

--- computer_ai.py
+++ computer_ai.py
@@ -2,38 +2,64 @@ from math import inf
 from copy import deepcopy
 
 
-def max_value(board):
+# Minimax functions with alpha-beta optimizing. No heuristics, so only three possible values: inf (win), -inf (lose), and tie (0).
+# These also take the markers ('X' or 'O') as arguments so that it can be run from either X's or O's perspective as the max value.
+# Further, any symbol could be used on the game board, instead of 'X' or 'O'.
+def max_value(board, good_marker, bad_marker, alpha=-inf, beta=inf):
     if not board.available_spaces:
         return 0
-    value = -inf
+    value = alpha
     for row, column in board.available_spaces:
         temp_board = deepcopy(board)
-        temp_board.make_move('X', row, column)
-        print(f'MAX PRINT\n{temp_board}')
-        if check_win(temp_board, 'X'):
-            print('MAX PRINT - win')
+        temp_board.make_move(good_marker, row, column)
+        if check_win(temp_board, good_marker):
             return inf
-        value = max(value, min_value(temp_board))
-    print('MAX PRINT', value)
+        value = max(value, min_value(
+            temp_board, good_marker, bad_marker, alpha, beta))
+        alpha = max(alpha, value)
+        if alpha >= beta:
+            break
     return value
 
 
-def min_value(board):
+def min_value(board, good_marker, bad_marker, alpha=-inf, beta=inf):
     if not board.available_spaces:
         return 0
-    value = inf
+    value = beta
     for row, column in board.available_spaces:
         temp_board = deepcopy(board)
-        temp_board.make_move('O', row, column)
-        print(f'MIN PRINT\n{temp_board}')
-        if check_win(temp_board, 'O'):
-            print('MIN PRINT - win')
+        temp_board.make_move(bad_marker, row, column)
+        if check_win(temp_board, bad_marker):
             return -inf
-        value = min(value, max_value(temp_board))
-    print('MIN PRINT', value)
+        value = min(value, max_value(
+            temp_board, good_marker, bad_marker, alpha, beta))
+        beta = min(beta, value)
+        if alpha >= beta:
+            break
     return value
 
 
+# Minimax will only be called from this next function. This may be somewhat redundant.
+# However, we need a board position for the next move to be returned, not just the value of the line of play.
+# Since we don't care about the board positions for plays on deeper plys during the minimax recursion,
+# and since it would hurt readability to have row and column variables being thrown back up the stack in the recursive call,
+# I decided to separate the root node of the game tree, making it a simple iteration that saves the row and column associated with the best value play.
+def decide_next_move(board, good_marker, bad_marker):
+    best_option = [-inf, (None, None)]
+    for row, column in board.available_spaces:
+        considering = deepcopy(board)
+        considering.make_move(good_marker, row, column)
+        result = min_value(considering, good_marker, bad_marker)
+        if result > best_option[0]:
+            best_option = [result, (row, column)]
+            if best_option[0] == inf:
+                break
+    return best_option[1]
+
+
+# This is a weak solution for check win state.
+# It's the same as simply run 8 if true statements for the 8 possible 3-in-a-rows. I just made it a little bit cuter.
+# I intend on increasing the intelligence of this, so that it can handle larger boards and varying lengths for the winning run.
 def check_win(board, marker):
     winning_rows = [[(0, 0), (0, 1), (0, 2)], [
         (1, 0), (1, 1), (1, 2)], [(2, 0), (2, 1), (2, 2)]]
